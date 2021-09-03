06145ccf8a44e36cff4b0842c9c9b922d787ef03
Anonim Education <soraplay111@gmail.com>
2021-09-03 16:11:52 +0300

    Добавьте файлы проекта.

---
+++ Dadadadadad/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Dadadadadad
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int x = 10;
+            for (int i = 0; i < x; i++)
+            {
+
+            }
+        }
+    }
+}
